fac8d38191ddacea141a8ab45701145115809110
Ryan Detres <rmdetres@gmail.com>
2022-11-29 10:48:32 -0600

    adding code

---
+++ README.md
@@ -0,0 +1,5 @@
+# module9hw
+
+## creates a readme file for the user
+
+## includes basic info on any given project
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,55 @@
+// function to generate markdown for README
+function generateMarkdown(data) {
+  const licenseLowerCase = data.license.toLowerCase();
+  return `
+  # ${data.title} ${data.version}
+  ![Badge](https://img.shields.io/badge/license-${data.license}-green)
+
+  ## Description
+
+  ${data.description}
+
+  Deployed Application: [${data.url}](${data.url})
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## License
+
+  Copyright &#169; 2020 [@${data.username}](https://github.com/${data.username}).<br>
+  This project is [${data.license}](https://choosealicense.com/licenses/${licenseLowerCase}/) licensed.
+  
+  ## Contributing
+
+  ${data.contributing}
+
+  ## Tests
+
+  ${data.tests}
+
+  ## Questions
+
+  [@${data.username}](https://github.com/${data.username})<br>
+  Please email me with any questions! <${data.email}>
+
+  _____________________________________________________
+  > 
+
+`;
+}
+
+module.exports = generateMarkdown;
